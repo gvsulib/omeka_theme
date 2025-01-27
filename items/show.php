6281448c38d7a4c31521528ece4c03bfb945c4d6
Matthew Reidsma <reidsmam@gvsu.edu>
2025-01-27 12:06:30 -0500

    Redesign item record page

--- items/show.php
+++ items/show.php
@@ -2,7 +2,15 @@
 
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
 <div id="primary">
-    <h1 class="h2"><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
+      <div class="wrapper">
+            <div class="content">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="h2"><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
 	<?php
 	$format = metadata('item', array('Dublin Core','Format'));
 	$collection = metadata('item', 'Collection Name');
@@ -22,7 +30,7 @@
         ?>
         <div class="wrapper">
             <div class="content">
-                <div class="row">
+                <div class="row-gutter">
                     <div class="col-6 col-sm-12">
                         <div id="item-images">
 	
@@ -41,13 +49,6 @@
                         <div id="item-metadata">
                             <?php echo all_element_texts('item'); ?>
                         </div>
-
-                       <?php if(metadata('item','Collection Name')): ?>
-                          <div id="collection" class="element">
-                            <h3><?php echo __('Collection'); ?></h3>
-                            <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
-                          </div>
-                       <?php endif; ?>
 
                      <!-- The following prints a list of all tags associated with the item -->
                     <?php if (metadata('item','has tags')): ?>
@@ -72,8 +73,9 @@
 
                 <ul class="item-pagination">
                     <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+                     <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
                     <li id="current-collection" style="text-align: center;"> All items in <?php echo link_to_collection_for_item(); ?></li>
-                    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+                   
                 </ul>
             </div>
         </div>
