46849503e1c10e9781ce0518aa50d89974cfe336
Matthew Reidsma <reidsmam@gvsu.edu>
2025-01-27 10:51:09 -0500

    Redesign item record page

--- items/show.php
+++ items/show.php
@@ -70,9 +70,9 @@
         <div class="wrapper-full-width wrapper-background wrapper-light">
             <div style="background-color: #f7f7f7;" class="row content">
 
-                <ul class="item-pagination navigation">
+                <ul class="item-pagination">
                     <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-                    <li id="current-collection"><?php echo link_to_collection_for_item(); ?></li>
+                    <li id="current-collection" style="text-align: center;"> All items in <?php echo link_to_collection_for_item(); ?></li>
                     <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
                 </ul>
             </div>
